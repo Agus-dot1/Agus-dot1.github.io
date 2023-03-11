bfab6aef2f6db93d39d09994b73ab7e7758cbdf0
Agusdev <agus1dev@gmail.com>
2023-03-11 02:08:06 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+$(document).ready(function () {
+  var ir_a = $(".desplazar");
+  ir_a.click(function (evento) {
+    evento.preventDefault();
+    $("body, html").animate(
+      {
+        scrollTop: $(this.hash).offset().top,
+      },
+      50
+    );
+  });
+});
