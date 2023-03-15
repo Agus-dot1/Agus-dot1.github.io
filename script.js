c8c8c3a493d52a582b88e5a5639bc047e4ab8b21
Agusdev <agus1dev@gmail.com>
2023-03-15 11:35:13 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader-hidden")
+
+  loader.addEventListener("transitionend"), () => {
+    document.body.removeChild("loader");
+  }
+})
+
+
 //Object animation when scrolling:
 
 const observer = new IntersectionObserver((entries) => {
